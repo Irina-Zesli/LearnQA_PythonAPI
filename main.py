78dbef9b23121a3f69ee33bb9cea862d04726a04
irina <zesli@mail.ru>
2021-06-24 18:39:16 +0300

    ошибка при парсинге json

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # print("Hello, world!")
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# payload = {"name": "User"}
+# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# parsed_response_text = response.json()
+# print(parsed_response_text["answer"])
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+parsed_response_text = response.json()
+print(parsed_response_text)
+
+
+
