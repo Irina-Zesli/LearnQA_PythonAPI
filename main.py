3d1c1ac0e04a36b67c2a6877b827221f9ba086a6
Ирина Зесли <zesli@mail.ru>
2021-06-26 20:48:59 +0300

    Перехват исключения, если не JSON

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# print("Hello, world!")
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+try:
+    parced_response_text = response.json()
+    print(parced_response_text)
+except JSONDecodeError:
+    print("Response is not a JSON format")
+
