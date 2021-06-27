9e5ce2998bdda7f96ab2cb27ef37ff1c530dba1c
Ирина Зесли <zesli@mail.ru>
2021-06-27 08:22:24 +0300

    Задание 6 (итоговый url)

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # from json.decoder import JSONDecodeError
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param1": "value1"})
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+print(response.status_code)
+first_response = response.history[0]
+second_response = response
+print(first_response.url)
+print(second_response.url)
+# print(response.text)
 
 
