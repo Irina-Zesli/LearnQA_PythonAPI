4633432543fbb8356387d84a29e76f5e0ee418f9
Ирина Зесли <zesli@mail.ru>
2021-07-01 18:35:27 +0300

    Параметризация тестов

--- test_first_api.py
+++ test_first_api.py
@@ -1,15 +1,24 @@
+import pytest
 import requests
 
 class TestFirstAPI:
-    def test_hello_call(self):
+    names = [
+        ("Vitalii"),
+        ("Arsenii"),
+        ("")
+    ]
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Vitalii'
         data = {'name': name}
 
         response = requests.get(url,params=data)
         assert response.status_code == 200, "Wrong response code"
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response"
-        expected_response_text = f"Hello, {name}"
+        if len(name)==0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
         actual_respnse_text = response_dict["answer"]
         assert actual_respnse_text == expected_response_text, "Actual text in the response is not correct"
