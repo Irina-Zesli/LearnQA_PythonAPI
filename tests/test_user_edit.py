0e53797ee639026d2edc636f33c7a56d13b3eba1
Ирина Зесли <zesli@mail.ru>
2021-07-12 22:49:16 +0300

    дз17 еще не доделано

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -3,6 +3,7 @@ from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 class TestUserEdit(BaseCase):
+
     def test_edit_just_created_user(self):
         # REGISTER
         register_data = self.prepare_registration_data()
@@ -16,7 +17,6 @@ class TestUserEdit(BaseCase):
         user_id = self.get_json_value(response1, "id")
 
         #LOGIN
-
         login_data = {
             'email': email,
             'password': password
@@ -51,8 +51,48 @@ class TestUserEdit(BaseCase):
             f"Wrong name of the user after edit"
         )
 
+    def test_edit_not_auth_user(self):
+        new_name = "changed_name"
+        response = requests.put("https://playground.learnqa.ru/api/user/2", data={"firstName": new_name})
+        Assertions.assert_code_status(response, 400)
+        #print(response2.status_code)
+        #print(response2.content)
+        Assertions.assert_content(response, "Auth token not supplied")
+
+    def test_edit_auth_as_another_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user", data=register_data)
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        # first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
 
 
+        # EDIT
+        new_name = "changed_name"
+
+        response3 = requests.put("https://playground.learnqa.ru/api/user/20200",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid},
+                                 data={"firstName": new_name}
+                                 )
+
+        print(response3.status_code)
+        print(response3.content)
 
 
 
